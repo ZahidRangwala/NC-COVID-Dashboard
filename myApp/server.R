dad1b65f72e0235aba06554cc80a3ef8635a2003
unknown <zahidrangwala3@gmail.com>
2020-08-12 13:05:19 -0400

    Added HTML Tags to all 4 tags , added confirmed and deaths box

--- myApp/server.R
+++ myApp/server.R
@@ -14,6 +14,15 @@ server <- function(input, output) {
   output$plotNCconfirmed <- renderPlot(ncMapConfirmed)
   
   confirmString <- paste("Total Confirmed: " , sum(dailyNC$Confirmed))
+  output$totalDeath <- renderText(confirmString)
+  
+  confirmString <- paste("Total Deaths: " , sum(dailyNC$Deaths))
+  output$totalRecovered <- renderText(confirmString)
+  
+  confirmString <- paste("Total Recovered: " , sum(dailyNC$Recovered))
+  output$totalActive <- renderText(confirmString)
+  
+  confirmString <- paste("Total Active: " , sum(dailyNC$Active))
   output$totalConfirm <- renderText(confirmString)
   
   output$plotNCdeath <-  renderPlot(ncMapDeath)
@@ -34,21 +43,43 @@ server <- function(input, output) {
   
   selectedCounty <- reactive({ input$county })
   
-  output$timeseriesNCconfirmed <-  renderPlot({
-    timeseriesNC %>% filter(Admin2 == selectedCounty()) %>%
-      filter(Confirmed != 0) %>% select(-Admin2) %>%
-      ggplot(aes(x = Date , y = Confirmed , group = 1)) + 
-      geom_line(color="firebrick") + geom_point(color="firebrick") +
-      ggtitle(paste("Cumulative cases in" , selectedCounty())) +
-      labs(x="Date", y=("Confirmed Cases")) +
-      theme(
-        plot.title = element_text(size=20, face="bold",
-        margin = margin(10, 0, 10, 0)) ,
-        axis.text.x=element_text(angle=60, vjust=0.5 , face = "bold") ,
-        axis.text.y=element_text(face = "bold"),
-        axis.title.x = element_text(vjust=-0.35 , face = "bold"),
-        axis.title.y = element_text(vjust=0.35 , face = "bold") 
-      )
+  observeEvent(input$displayConfirmed, {
+    output$timeseriesNCconfirmed <-  renderPlot({
+      timeseriesNC %>% filter(Admin2 == selectedCounty()) %>%
+        filter(Confirmed != 0) %>% select(-Admin2) %>%
+        ggplot(aes(x = Date , y = Confirmed , group = 1)) + 
+        geom_line(color="#8b0000") + geom_point(color="#8b0000") +
+        ggtitle(paste("Cumulative cases in" , selectedCounty())) +
+        labs(x="Date", y=("Confirmed Cases")) +
+        theme(
+          plot.title = element_text(size=20, face="bold",
+                                    margin = margin(10, 0, 10, 0)) ,
+          axis.text.x=element_text(angle=60, vjust=0.5 , face = "bold") ,
+          axis.text.y=element_text(face = "bold"),
+          axis.title.x = element_text(vjust=-0.35 , face = "bold"),
+          axis.title.y = element_text(vjust=0.35 , face = "bold") 
+        )
+    })
   })
+  
+  observeEvent(input$displayDeaths, {
+    output$timeseriesNCconfirmed <-  renderPlot({
+      timeseriesNC %>% filter(Admin2 == selectedCounty()) %>%
+        filter(Deaths != 0) %>% select(-Admin2) %>%
+        ggplot(aes(x = Date , y = Deaths , group = 1)) + 
+        geom_line(color="#00003b") + geom_point(color="#00003b") +
+        ggtitle(paste("Cumulative Deaths in" , selectedCounty())) +
+        labs(x="Date", y=("Confirmed Deaths")) +
+        theme(
+          plot.title = element_text(size=20, face="bold",
+                                    margin = margin(10, 0, 10, 0)) ,
+          axis.text.x=element_text(angle=60, vjust=0.5 , face = "bold") ,
+          axis.text.y=element_text(face = "bold"),
+          axis.title.x = element_text(vjust=-0.35 , face = "bold"),
+          axis.title.y = element_text(vjust=0.35 , face = "bold") 
+        )
+    })
+  })
+
 
 }
--- myApp/ui.R
+++ myApp/ui.R
@@ -1,50 +1,94 @@
 library(shinydashboard)
 library(shiny)
 
-sidebar <- dashboardSidebar(
-  sidebarMenu(
-    menuItem("North Carolina", tabName = "ncTab")
-  )
-)
+sidebar <- dashboardSidebar(sidebarMenu(menuItem("North Carolina", tabName = "ncTab")))
 
-body <- dashboardBody(
-  tabItems(
-    
-    tabItem(tabName = "ncTab",
-        tabBox(
-          id = "NCTab" , height = "450px" , width = "800px" ,
-          tabPanel(
-            "Confirmed" , 
-            textOutput("totalConfirm"),
-                tags$head(tags$style("#totalConfirm{
+body <- dashboardBody(tabItems(
+  tabItem(
+    tabName = "ncTab",
+    tabBox(
+      id = "NCTab" ,
+      height = "450px" ,
+      width = "800px" ,
+      tabPanel(
+        "Confirmed" ,
+        textOutput("totalConfirm"),
+        tags$head(
+          tags$style(
+            "#totalConfirm{
                                  text-align: center;
-                                 background-color: red;
+                                 background-color: #8b0000;
                                  border: 1px solid #000000;
                                  font-family: Helvetica;
                                  font-size: 20px;
                                  font-style: bold;}"
-                         )
-                ),
-            plotOutput("plotNCconfirmed")
-          ),
-          
-          
-          
-          tabPanel("Deaths" , plotOutput("plotNCdeath")),
-          tabPanel("Recovered" , plotOutput("plotNCrecoverd")),
-          tabPanel("Active" , plotOutput("plotNCactive"))
+          )
         ),
-        plotOutput("NCbargraph"),
-        uiOutput("ncCounty"),
-        plotOutput("timeseriesNCconfirmed"),
-        tableOutput("confirmedNCtable")
+        plotOutput("plotNCconfirmed")
+      ),
+      tabPanel(
+        "Deaths" ,
+        textOutput("totalDeath"),
+        tags$head(
+          tags$style(
+            "#totalDeath{
+                                 text-align: center;
+                                 background-color: #466085;
+                                 border: 1px solid #000000;
+                                 font-family: Helvetica;
+                                 font-size: 20px;
+                                 font-style: bold;}"
+          )
+        ),
+        plotOutput("plotNCdeath")
+      ),
+      tabPanel(
+        "Recovered" ,
+        textOutput("totalRecovered"),
+        tags$head(
+          tags$style(
+            "#totalRecovered{
+                                 text-align: center;
+                                 background-color: Green;
+                                 border: 1px solid #000000;
+                                 font-family: Helvetica;
+                                 font-size: 20px;
+                                 font-style: bold;}"
+          )
+        ),
+        plotOutput("plotNCrecoverd")
+      ),
+      
+      tabPanel(
+        "Active" ,
+        textOutput("totalActive"),
+        tags$head(
+          tags$style(
+            "#totalActive{
+                                 text-align: center;
+                                 background-color: Yellow;
+                                 border: 1px solid #000000;
+                                 font-family: Helvetica;
+                                 font-size: 20px;
+                                 font-style: bold;}"
+          )
+        ),
+        
+        
+        
+        plotOutput("plotNCactive")
       )
+    ),
+    plotOutput("NCbargraph"),
+    uiOutput("ncCounty"),
+    actionButton("displayConfirmed", "Confirmed"),
+    actionButton("displayDeaths", "Deaths"),
+    plotOutput("timeseriesNCconfirmed"),
+    tableOutput("confirmedNCtable")
   )
-)
+))
 
-dashboardPage(
-  skin = "purple" ,
-  dashboardHeader(title = "COVID-19 Dashboard"),
-  sidebar,
-  body
-)
+dashboardPage(skin = "purple" ,
+              dashboardHeader(title = "COVID-19 Dashboard"),
+              sidebar,
+              body)
